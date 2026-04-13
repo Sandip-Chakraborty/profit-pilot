c777c023f102528e33deef86431cbb8931ed6735
Sandip Chakraborty <sandip01998chakraborty@gmail.com>
2026-04-14 00:07:09 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 6
+  },
+  accountName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  referralCode: {
+    type: String,
+    default: ""
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
